3dc7c2aa693e56e7586f9c630025ed7499129514
Gaurav-Soni24 <sonigaurav2021@gmail.com>
2024-07-20 01:01:19 +0530

    Update styles and project data; add Portfolio image

--- Components/projectData.js
+++ Components/projectData.js
@@ -13,7 +13,15 @@ const projectData = [
         description: 'A simple website that provides canvas for drawing and saving image.',
         link: 'https://github.com/Gaurav-Soni24/DrawBoard',
         liveLink: 'https://gaurav-soni24.github.io/DrawBoard/'
+    },
+    {
+        image: '/Portfolio.jpg',
+        title: 'Portfolio',
+        description: 'My Portfolio as webpage  using NextJS.',
+        link: 'https://github.com/Gaurav-Soni24/GauravPortfolio',
+        liveLink: 'https://gaurav-portfolio-theta.vercel.app'
     }
+    
   ];
   
   export default projectData;
