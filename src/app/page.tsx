b5534f0a3af3668094820f3930eaf80ef242a4ca
Gaurav Soni <115176423+Gaurav-Soni24@users.noreply.github.com>
2026-03-27 18:43:53 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,20 @@ import Gaurav from '../components/Gaurav'
 import About from '../components/About'
 import Skill from '../components/Skill'
 import Qualification from '../components/Qualification'
+import Experience from '../components/Experience'
+import Projects from '../components/Projects';
 import Contact from '../components/Contact'
-import Projects from '@/components/Projects';
 
 export default function Home() {
-
   return (
     <>
       <section id="home"><Gaurav /></section>
       <section id="about"><About /></section>
       <section id="skills"><Skill /></section>
       <section id="qualification"><Qualification /></section>
+      <section id="experience"><Experience /></section>
       <section id="projects"><Projects /></section>
       <section id="contact"><Contact /></section>
-
     </>
   );
 }
